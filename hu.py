c411366213ed17ae5028edc46c3336f3b8c931d2
adinosRing <91734752+adinosRing@users.noreply.github.com>
2021-10-01 17:45:22 +0530

    Add files via upload

---
+++ hu.py
@@ -0,0 +1,24 @@
+print('-----------SIMPLE ---CALCULATOR-----------------')
+print('\n')
+print('\n')
+print('\n')
+
+x=int(input('Enter a number x: '))
+y=int(input('Enter a number y: '))
+
+#Addition
+add = x+y
+print('Addition = ',add)
+
+#substraction
+print('Substraction = ',x-y)
+
+#Multiplcation
+print('Multiplication = ',x*y)
+
+
+#Division
+print('Division = ',x/y)
+
+
+                                                                    
